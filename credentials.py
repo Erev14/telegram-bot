d3940b8de6226ac537c780538375c97b403e1848
Erev14 <erevjoach14@gmail.com>
2022-02-02 05:54:55 +0000

    fix fix misspelling

--- credentials.py
+++ credentials.py
@@ -2,13 +2,15 @@ import configparser
 
 config_parser = configparser.RawConfigParser()
 
+
 def save_credentials(dic: dict) -> None:
-    config_parser.add_section('credencials')
+    config_parser.add_section('credentials')
     for key, val in dic.items():
-        config_parser.set('credencials', key, val)
-    with open('config.ini', 'w') as setup:
+        config_parser.set('credentials', key, val)
+    with open('config.ini', 'w', encoding='UTF-8') as setup:
         config_parser.write(setup)
 
-def load_credentials() -> dict :
+
+def load_credentials() -> dict:
     config_parser.read('config.ini')
     return dict(config_parser['credentials'].items())
